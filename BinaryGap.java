33825c8884aad72c92e4e626261dd341f80c2008
KwonJH <hojak99@gmail.com>
2017-04-21 23:19:08 +0900

    Add BinaryGap
    미친 난 망했다

---
+++ BinaryGap.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Binary {
+	public static void main(String[] args){
+		System.out.println(solution(561892));
+	}
+	
+	public static int solution(int N){
+		
+		String binaryString = Integer.toBinaryString(N);
+		char[] binaryToChar = binaryString.toCharArray();
+		
+		
+		int tempLen = 0;
+		int maxLen = 0;
+		
+		
+		// 10001001001011100100
+		for(char a : binaryToChar){
+			if(String.valueOf(a).equals("1")){
+				if(tempLen >= maxLen){
+					maxLen = tempLen;
+					tempLen = 0;
+					System.out.println(maxLen);
+				}else{
+					tempLen =0;
+				}
+			}else{
+				tempLen += 1;
+			}
+		}
+		
+		return maxLen;
+	}
+}
