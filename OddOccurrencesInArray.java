92346ff6045f7ecc76605a9ab37943659c331df0
KwonJH <hojak99@gmail.com>
2017-04-22 20:18:07 +0900

    Improve algorithm
    미친 xor 혁신이야

--- OddOccurrencesInArray.java
+++ OddOccurrencesInArray.java
@@ -1,25 +1,20 @@
 public class OddOccurrencesInArray {
 	public static void main(String[] args) {
 		int aa[] = {9,3,9,3,9,7,9};
+		
 		System.out.println(solution(aa));
 	}
 	
 	public static int solution(int[] A) {
 		
-		int count = 0;
+		int temp = 0;
 		
-		for(int q= 0; q<A.length; ++q){
-			for(int i=q+1; i<A.length; ++i){
-				if(A[q] == A[i]){
-					A[q] = -1;
-					A[i] = -1;
-				}
-			}
-			if (A[q] != -1) {
-				count = A[q];
-			}
+		for(int i= 0; i<A.length; ++i){
+			temp = temp ^ A[i];
+			System.out.println("xor : "+temp);
 		}
-		
-		return count;
+		return temp;
 	}
+	
+	// 시간 복잡도 실화냐?
 }
