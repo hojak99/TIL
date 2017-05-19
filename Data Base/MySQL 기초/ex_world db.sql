34bcdba31cb69c63b564e80e4b1e9156e39668b8
KwonJH <hojak99@gmail.com>
2017-05-19 17:25:24 +0900

    world DB 사용 방법 및 쿼리

---
+++ Data Base/MySQL 기초/ex_world db.sql
@@ -0,0 +1,14 @@
+# 현재 존재하는 데이터베이스 리스트 출력
+show databases;
+
+# "world" 데이터베이스 사용
+use world;
+
+# "world" DB의 테이블 리스트 출력
+show tables;
+
+# "world" DB의 "city" 테이블 구조 출력
+desc city;
+
+# "world" DB의 "city" 테이블의 모든 컬럼 출력 
+select * from city;
