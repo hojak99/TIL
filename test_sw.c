5b1aa624730521ef3163e44784cda640e3db017e
KwonJH <hojak99@gmail.com>
2017-05-31 02:10:26 +0000

    Add switch code in led code

--- test_sw.c
+++ test_sw.c
@@ -1,13 +1,58 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <wiringPi.h>
+
+#define LED 1
 
 int readSwitch(int gpio)
 {
+	int value = 0;
 	int fd;
+	pinMode(LED, OUTPUT);
+
 	char buf[BUFSIZ];
 	fd = open("/sys/class/gpio/export", O_WRONLY);
 	sprintf(buf, "%d", gpio);
 	write(fd,buf, strlen(buf));
 	close(fd);
+
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);
+	printf("\n ..... %s ..... \n", buf);
+	fd = open(buf, O_RDONLY);
+	read(fd, &value, 1);
+	
+	printf("value : %d %c", value, value);
+
+	if(value == '0') {
+		printf("\n ======= Switch Pressed ========\n");
+		digitalWrite(LED, HIGH);
+	}else {
+		printf("\n ......  Switch Released ....... ");
+		digitalWrite(LED, LOW);
+	}
+
+	close(fd);
+	getchar();
+
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	sprintf(buf, "%d", gpio);
+	write(fd, buf, strlen(buf));
+	close(fd);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int gno;
+	if(argc < 2) {
+		printf("Usage : %s SW_GPIO_NO", argv[0]);
+		return (-1);
+	}
+
+	wiringPiSetup();
+	gno = atoi(argv[1]);
+	readSwitch(gno);
+	return 0;
 }
