6206bf95ddb9303e02e312a1fb8276d182e67986
KwonJH <hojak99@gmail.com>
2017-07-04 23:09:36 +0900

    Spring 추가

---
+++ WEB/Spring/토비의 스프링/1장/DAO_Test/src/UserDao.java
@@ -0,0 +1,47 @@
+import java.nio.file.attribute.UserPrincipalLookupService;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = DriverManager.getConnection("jdbc::oracle:thin:@localhost:1521:orcl", "hojak99", "0430");
+		
+		PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO users(id, name, password) values(?, ?, ?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeQuery();
+		ps.close();
+		connection.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection = DriverManager.getConnection("jdbc::oracle:thin:@localhost:1521:orcl", "hojak99", "0430");
+		
+		PreparedStatement ps = connection.prepareStatement(
+				"SELECT * FROM users WHERE id = ?"
+		);
+		ps.setString(1, id);
+	
+		ResultSet resultSet = ps.executeQuery();
+		resultSet.next();
+		
+		User user = new User();
+		user.setId(resultSet.getString("id"));
+		user.setName(resultSet.getString("name"));
+		user.setPassword(resultSet.getString("password"));
+		
+		resultSet.close();
+		ps.close();
+		connection.close();
+		
+		return user;
+	}
+}
