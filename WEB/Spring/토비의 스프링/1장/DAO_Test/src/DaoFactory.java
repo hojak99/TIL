b3862d9b068fde7176c1c51e58394432c89bc45d
KwonJH <hojak99@gmail.com>
2017-07-08 19:44:08 +0900

    DaoFactory 를 사용하는 애플리케이션 컨텍스트

--- WEB/Spring/토비의 스프링/1장/DAO_Test/src/DaoFactory.java
+++ WEB/Spring/토비의 스프링/1장/DAO_Test/src/DaoFactory.java
@@ -1,23 +1,32 @@
-import java.sql.Connection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
+	
+	@Bean
 	public UserDao userDaoD(){
 		return new UserDao(connectionMaker());
 	}
 	
+	@Bean
 	public UserDao userDaoN(){
 		return new UserDao(connectionMaker());
 	}
 	
+	@Bean
 	public AccountDao accountDao(){
 		return new AccountDao(connectionMaker());
 	}
 	
+	@Bean
 	public MessageDao messageDao(){
 		return new MessageDao(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker(){
 		return new DConnectionMaker();
 	}
+
 }
--- WEB/Spring/토비의 스프링/1장/DAO_Test/src/UserDaoTest.java
+++ WEB/Spring/토비의 스프링/1장/DAO_Test/src/UserDaoTest.java
@@ -1,11 +1,22 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.Annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao daoD = new DaoFactory().userDaoD();
-		UserDao daoN = new DaoFactory().userDaoN();
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		
+		/*
+		 * getBean() 메소드는 ApplicationContext 가 관리하는 오브젝트를 요청하는 메소드이다.
+		 * "userDao" 는 ApplicationContext 에 등록된 빈의 이름
+		 * */
+		UserDao dao = context.getBean("userDao", UserDao.class);
 	}
 
 }
