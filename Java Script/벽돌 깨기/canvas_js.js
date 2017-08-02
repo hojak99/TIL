6b931e101c2862059982ab43969cb1c62c9b1aed
KwonJH <hojak99@gmail.com>
2017-08-02 15:17:47 +0900

    공 튕기도록 하고 튕길 때마다 색상 변경

--- Java Script/벽돌 깨기/canvas_js.js
+++ Java Script/벽돌 깨기/canvas_js.js
@@ -1,20 +1,41 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+var ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(160,10,100,40);
-ctx.strokeStyle = "rgba(0,0,255,0.5)";
-ctx.stroke();
-ctx.closePath();
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+        dx = -dx;
+        changeBallColor("#FF00DD");
+    }
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+        dy = -dy;
+        changeBallColor("#3F0099");
+    }
+
+    x += dx;
+    y += dy;
+}
+
+function changeBallColor(code){
+    ctx.fillStyle = code;
+    ctx.fill();
+}
+
+setInterval(draw, 10);
