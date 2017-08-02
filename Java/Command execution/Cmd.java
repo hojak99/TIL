0be84204bd9e3497d68c20893fe338c11d250fcb
KwonJH <hojak99@gmail.com>
2017-08-02 09:19:19 +0900

    파일 삭제 코드 추가

--- Java/Command execution/Cmd.java
+++ Java/Command execution/Cmd.java
@@ -1,5 +1,8 @@
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Cmd {
@@ -9,20 +12,50 @@ public class Cmd {
 	private BufferedReader bufferedReader;
 	private StringBuffer readBuffer;
 	
-	public String inputCommand(String cmd) {
+	private File file;
+	private BufferedWriter bufferWriter;
+	
+	private final String FILENAME = "Test.java";
+	
+	public String inputSource(String source) {
 		
 		buffer = new StringBuffer();
 		
 		buffer.append("cmd.exe ");	
-		buffer.append("/c ");
-		buffer.append(cmd);
+		buffer.append("/c  ");
+		buffer.append("javac Test.java");
+		
+		createFileAsSource(source);
 
 		return buffer.toString();
+	}
+	
+	private void createFileAsSource(String source) {
+		try {
+			file = new File(FILENAME);
+			bufferWriter = new BufferedWriter(new FileWriter(file, false));
+			
+			bufferWriter.write(source);
+			bufferWriter.flush();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}finally {
+			try {
+				bufferWriter.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 	}
 	
 	public String execCommand(String cmd) {
 		try {
 			process = Runtime.getRuntime().exec(cmd);
+			process = Runtime.getRuntime().exec(runClass());
+			
 			bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			
 			String line = null;
@@ -33,12 +66,22 @@ public class Cmd {
 				readBuffer.append("\n");
 			}
 			
+			file.delete();
 			return readBuffer.toString();
 		}catch (Exception e) {
 			e.printStackTrace();
-			System.exit(1);
 		}
 		
 		return null;
+	}
+	
+	private String runClass() {
+		buffer = new StringBuffer();
+		
+		buffer.append("cmd.exe ");	
+		buffer.append("/c ");
+		buffer.append("java Test");
+
+		return buffer.toString();
 	}
 }
