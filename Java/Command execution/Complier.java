1704471a522cf09dca341219b38bd71b71c9637b
KwonJH <hojak99@gmail.com>
2017-08-02 09:41:45 +0900

    입력한 소스 코드를 컴파일 해 실행하도록 함

--- Java/Command execution/Complier.java
+++ Java/Command execution/Complier.java
@@ -3,7 +3,12 @@ public class Complier {
 	public static void main(String[] args) {
 		Cmd cmd = new Cmd();
 	
-		String command = cmd.inputCommand("ipconfig");
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("public class Test");
+		buffer.append("{ public static void main(String[] args)");
+		buffer.append("{ System.out.println(\"sdadadasdadasdasd\");} }");
+		
+		String command = cmd.inputSource(buffer.toString());
 		String result = cmd.execCommand(command);
 		
 		System.out.println(result);
