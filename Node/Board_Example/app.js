2139f336d98be717bda24ae3e3eb0a74c113cabb
KwonJH <hojak99@gmail.com>
2017-12-14 23:53:06 +0900

    node에 mysql 연결

--- Node/Board_Example/app.js
+++ Node/Board_Example/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var mysql_dbc = require('../Board_Example/db/db_con')();
 var app = express();
+var connection = mysql_dbc.init();
 
 app.get('/', function(req, res) {
     res.send('Hello World');
@@ -11,3 +13,5 @@ var server = app.listen(3000, function() {
 
     console.log('앱은 http://%s:%s 에서 작동 중입니다.', host, port);
 });
+
+mysql_dbc.test_open(connection);
---
+++ Node/Board_Example/db/db_con.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+
+module.exports = function() {
+    return {
+        init : function () {
+            return mysql.createConnection({
+                host: 'localhost',
+                port: '3306',
+                user: 'root',
+                password : '0430',
+                database : 'board_node'
+            })
+        },
+
+        test_open: function(con) {
+            con.connect(function(err) {
+                if(err) {
+                    console.log('mysql connection error : '+ err);
+                } else {
+                    console.log('mysql is connected successfully');
+                }
+            })
+        }
+    }
+};
