79e94fff8f8819355177e56f14c558193d682743
KwonJH <hojak99@gmail.com>
2018-04-30 14:33:00 +0900

    Spring WebFlux 주석 추가

--- Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/ExampleController.java
+++ Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/ExampleController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleController {
 	
+	/**
+	 * 기존의 어노테이션을 사용한 컨트롤러 메소드
+	 * 
+	 * @return
+	 */
 	@PostMapping(path="/", consumes = {MediaType.APPLICATION_JSON_VALUE, "!application/xml"},
 			produces = MediaType.TEXT_PLAIN_VALUE, headers = "X-Custom=Foo", params = "a!=alpha")
 	public String example() {
--- Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/WelcomeController.java
+++ Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/WelcomeController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+	
+	/**
+	 * 기존의 어노테이션을 이용한 컨트롤러 메소드
+	 * 
+	 * @return
+	 */
 	@GetMapping("/")
 	public String welcome() {
 		return "Hello World";
