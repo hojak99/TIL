3e1015196e87c0814cacfdd81ab988dcae4fd8bf
KwonJH <hojak99@gmail.com>
2018-04-30 14:28:11 +0900

    Spring WebFlux 주석 추가

--- Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/SpringWebFluxApplication.java
+++ Project/Spring/Spring-WebFlux/src/main/java/com/kjh/hojak/SpringWebFluxApplication.java
@@ -16,7 +16,15 @@ public class SpringWebFluxApplication {
 		SpringApplication.run(SpringWebFluxApplication.class, args);
 	}
 
+
 	
+	/**
+	 * 새로운 방식의 routing 방법. 좀 더 functional 한 프로그래밍을 할 수 있다. 
+	 * 쉽게 생각하면 URL을 명시하는 RouterFunction 과 URL과 로직을 연결하는 HandlerFunction 으로 나눌 수 있다.
+	 * 
+	 * @param echoHandler
+	 * @return
+	 */
 	@Bean
 	public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
 		return route(POST("/echo"), echoHandler::echo);
