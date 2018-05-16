56265fd694a1d8a6bd6b0a07a28a345790660345
KwonJH <hojak99@gmail.com>
2018-05-16 19:03:30 +0900

    Spring boot JPA 예제 프로젝트 생성

---
+++ Project/Spring/Spring-Boot-Samples/Spring-Boot-Sample-JPA/src/main/java/com/kjh/hojak/SpringBootSampleJpaApplication.java
@@ -0,0 +1,12 @@
+package com.kjh.hojak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleJpaApplication.class, args);
+	}
+}
