375e96ce50fea9cc470924e5343db6e9cae38118
KwonJH <hojak99@gmail.com>
2018-05-21 14:50:41 +0900

    Spring-Boot-Sample-JPA : BoardRepository 반환 객체 변경 및 위치 변경

---
+++ Project/Spring/Spring-Boot-Samples/Spring-Boot-Sample-JPA/src/main/java/com/kjh/hojak/repository/BoardRepository.java
@@ -0,0 +1,25 @@
+package com.kjh.hojak.repository;
+
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kjh.hojak.domain.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Long>{
+	
+	/**
+	 * writer_id 로 Board 찾기
+	 * @return {@link Page}
+	 */
+	Page<Board> findByWriterId(String writer_id, Pageable pageable);
+	
+	
+	/**
+	 * 모든 Board 가져오기
+	 * @return {@link Page}
+	 */
+	Page<Board> findAll(Pageable pageable);
+}
