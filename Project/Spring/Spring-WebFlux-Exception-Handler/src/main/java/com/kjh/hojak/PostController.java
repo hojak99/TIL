86474dd539bbcbf49cbccbfbe059d411b98b1bc8
KwonJH <hojak99@gmail.com>
2018-05-25 21:56:02 +0900

    Interview.md : 정규화 예제 추가

---
+++ Project/Spring/Spring-WebFlux-Exception-Handler/src/main/java/com/kjh/hojak/PostController.java
@@ -0,0 +1,5 @@
+package com.kjh.hojak;
+
+public class PostController {
+
+}
---
+++ Project/Spring/Spring-WebFlux-Exception-Handler/src/main/java/com/kjh/hojak/SpringWebFluxExceptionHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.kjh.hojak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxExceptionHandlerApplication.class, args);
+	}
+}
