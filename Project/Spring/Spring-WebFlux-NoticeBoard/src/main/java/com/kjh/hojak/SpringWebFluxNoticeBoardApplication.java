7051fbe5697de2b6ab4a5ccf1df5af93cd10668a
KwonJH <hojak99@gmail.com>
2018-05-31 20:30:08 +0900

    Spring-WebFlux-NoticeBoard 프로젝트 셋팅

---
+++ Project/Spring/Spring-WebFlux-NoticeBoard/src/main/java/com/kjh/hojak/SpringWebFluxNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package com.kjh.hojak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxNoticeBoardApplication.class, args);
+	}
+}
