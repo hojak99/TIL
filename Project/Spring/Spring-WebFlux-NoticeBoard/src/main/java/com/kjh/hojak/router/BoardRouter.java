e6b1f4dca584aca64cd0c6053a436824fa01a271
KwonJH <hojak99@gmail.com>
2018-06-01 01:16:05 +0900

    Spring-WebFlux-NoticeBoard : Board save 라우팅

--- Project/Spring/Spring-WebFlux-NoticeBoard/src/main/java/com/kjh/hojak/router/BoardRouter.java
+++ Project/Spring/Spring-WebFlux-NoticeBoard/src/main/java/com/kjh/hojak/router/BoardRouter.java
@@ -7,19 +7,23 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.assertj.core.api.Assertions.setRemoveAssertJRelatedElementsFromStackTrace;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 import com.kjh.hojak.handler.BoardHandler;
+import com.kjh.hojak.model.Board;
 
 @Configuration
 public class BoardRouter {
 	@Bean
 	RouterFunction<ServerResponse> boardAPI(BoardHandler handler) {
 		return RouterFunctions.
-				route(GET("/boars").and(accept(MediaType.APPLICATION_JSON)),
-					handler::fetchAll);
+				route(GET("/boards").and(accept(MediaType.APPLICATION_JSON)),
+					handler::fetchAll)
+				.andRoute(POST("/board").and(accept(MediaType.APPLICATION_JSON)), 
+						request -> handler.save(request.bodyToMono(Board.class)));
 	}
 }
