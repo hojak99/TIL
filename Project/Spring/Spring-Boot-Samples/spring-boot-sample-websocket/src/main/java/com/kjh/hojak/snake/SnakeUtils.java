281b56853be90f033e6692061fcd5471ed96450d
KwonJH <hojak99@gmail.com>
2018-06-27 18:48:33 +0900

    spring-boot-sample-websocket : SnakeUtils 클래스 생성

---
+++ Project/Spring/Spring-Boot-Samples/spring-boot-sample-websocket/src/main/java/com/kjh/hojak/snake/SnakeUtils.java
@@ -0,0 +1,35 @@
+package com.kjh.hojak.snake;
+
+import java.awt.*;
+import java.util.Random;
+
+public class SnakeUtils {
+    public static final int PLAYFIELD_WIDTH = 640;
+    public static final int PLAYFIELD_HEIGHT = 480;
+    public static final int GRID_SIZE = 10;
+
+    private static final Random random = new Random();
+
+    public static String getRandomHexColor() {
+        float hue = random.nextFloat();
+        // 0.1, 0.3 사이
+        float saturation = (random.nextInt(2000) + 1000) /10000f;
+        float luminance = 0.9f;
+
+        Color color = Color.getHSBColor(hue, saturation, luminance);
+        return '#'+Integer.toHexString((color.getRGB() & 0xffffff) | 0x1000000).substring(1);
+    }
+
+    public static Location getRandomLocation() {
+        int x = roundByGridSize(random.nextInt(PLAYFIELD_WIDTH));
+        int y = roundByGridSize(random.nextInt(PLAYFIELD_HEIGHT));
+        return new Location(x, y);
+    }
+
+    private static int roundByGridSize(int value) {
+        value = value + (GRID_SIZE / 2);
+        value = value / GRID_SIZE;
+        value = value * GRID_SIZE;
+        return value;
+    }
+}
