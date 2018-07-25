981e15b733ab14816d2ad7be0f6ee8027b8d609e
KwonJH <hojak99@gmail.com>
2018-07-25 22:02:32 +0900

    vue : todo 프로젝트 셋팅

---
+++ Project/Vue.js/todos-client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <button class="btn btn-primary">Button</button>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ Project/Vue.js/todos-client/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example
+    },
+    {
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
+    }
+  ]
+})
