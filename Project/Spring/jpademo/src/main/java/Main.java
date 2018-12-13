820b01e9d0c824d693dbf11c8d1571983344e985
hojak99 <hojak99@gmail.com>
2018-12-13 20:47:50 +0900

    EntityMangerFactory 생성

--- Project/Spring/jpademo/src/main/java/Main.java
+++ Project/Spring/jpademo/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args){
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        entityManagerFactory.close();
+
         System.out.println("hello world");
     }
 }
