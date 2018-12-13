3d892140a099b0ffa3142ab1ba9d3c45bcdf0ccc
hojak99 <hojak99@gmail.com>
2018-12-13 20:54:11 +0900

    insert 코드 추가

--- Project/Spring/jpademo/src/main/java/Entity/Member.java
+++ Project/Spring/jpademo/src/main/java/Entity/Member.java
@@ -10,4 +10,20 @@ public class Member {
 
 
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Project/Spring/jpademo/src/main/java/Main.java
+++ Project/Spring/jpademo/src/main/java/Main.java
@@ -1,11 +1,31 @@
+import Entity.Member;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args){
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-        entityManagerFactory.close();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("호장권");
+
+            entityManager.persist(member);
+
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+        }finally {
+            entityManagerFactory.close();
+        }
 
         System.out.println("hello world");
     }
